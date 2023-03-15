6e003a658ded02b98035b0b0bfa20cf848c02f05
unknown <mb.2buis@yahoo.com>
2023-03-15 09:06:14 +0530

    fetched and displayed the data from api

---
+++ src/api/tvmaze.js
@@ -0,0 +1,16 @@
+const BASE_URL = 'https://api.tvmaze.com';
+
+export const apiGet = async queryString => {
+  const response = await fetch(`${BASE_URL}${queryString}`);
+  const body = await response.json();
+
+  return body;
+};
+
+// /search/shows?q=${queryURL} <== queryString
+
+//compressing the above even furhter
+// export const searchForShows = query => {
+//    return apiGet(`/search/shows?q=${query}`);
+// };
+export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { searchForShows } from '../api/tvmaze';
 
 function Home() {
   const [searchString, setSearchString] = useState('');
+  const [apiData, setApiData] = useState([]);
+  const [apiDataError, setApiDataError] = useState(null);
   console.log(searchString);
+  console.log('error at API: ', apiDataError);
 
   const onSearchInputChange = e => {
     // console.log('event: ', e);
-    console.log('targeted value: ', e.target.value);
+    // console.log('targeted value: ', e.target.value);
     setSearchString(e.target.value); //setting the input value here
   };
 
@@ -18,13 +22,40 @@ function Home() {
     //   .then(response => response.json())
     //   .then(body => console.log(body));
 
+    // /search/shows?q=:query
     //getting the response
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${searchString}`
-    );
-    const body = await response.json();
+    // const response = await fetch(
+    //   `https://api.tvmaze.com/search/shows?q=${searchString}`
+    // );
+    // const body = await response.json();
 
-    console.log(body);
+    // const body = await apiGet(`/search/shows?q=${searchString}`)
+
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchString);
+      setApiData(result);
+      console.log(result);
+      console.log('api: ', apiData);
+    } catch (error) {
+      setApiDataError(error);
+      console.log('error at API: ', apiDataError);
+    }
+  };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>This happened: {apiDataError.message}! </div>;
+    }
+
+    if (apiData) {
+      //returns do two things: cancelling off and giving it out the result
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name} </div>
+      ));
+    }
+
+    return null;
   };
 
   return (
@@ -39,8 +70,9 @@ function Home() {
           value={searchString}
           onChange={onSearchInputChange}
         />
-        <button type="submit">Random Change</button>
+        <button type="submit">Submit</button>
       </form>
+      <div>{renderApiData()}</div>
     </div>
   );
 }
