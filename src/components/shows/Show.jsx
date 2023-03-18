bf49752605ef928b37c821733403669bdf73971f
unknown <mb.2buis@yahoo.com>
2023-03-18 06:54:43 +0530

    added react query and replaced useShowById with useQuery Hook

--- src/components/shows/Show.jsx
+++ src/components/shows/Show.jsx
@@ -1,36 +1,40 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../../api/tvmaze';
+import { useQuery } from '@tanstack/react-query';
+// const useShowById = showId => {
+//   const [showData, setShowData] = useState(null);
+//   const [showError, setShowError] = useState(null);
+//   console.log(showId);
 
-const useShowById = showId => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-  console.log(showId);
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const data = await getShowById(showId);
+//         console.log('data: ', data);
+//         setShowData(data);
+//         console.log(showData);
+//         console.log('showData: ', showData);
+//       } catch (error) {
+//         setShowError(error);
+//         console.log(showError);
+//       }
+//     }
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        console.log('data: ', data);
-        setShowData(data);
-        console.log(showData);
-        console.log('showData: ', showData);
-      } catch (error) {
-        setShowError(error);
-        console.log(showError);
-      }
-    }
+//     fetchData();
+//   }, [showId]);
 
-    fetchData();
-  }, [showId]);
-
-  return { showData, showError };
-};
+//   return { showData, showError };
+// };
 
 export const Show = () => {
   // const params = useParams();
   const { showId } = useParams();
-  const { showData, showError } = useShowById(showId);
+  // const { showData, showError } = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', showId], //this like the dependency array
+    queryFn: () => getShowById(showId), //async await function call is this one
+  });
 
   if (showError) {
     return <div> We have an error!! {showError.message}</div>;
