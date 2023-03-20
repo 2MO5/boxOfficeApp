2c956d81474b1f1874a069b0baada09047a56674
unknown <mb.2buis@yahoo.com>
2023-03-20 09:36:52 +0530

    eslint changes, fetched data for starred and displayed it

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -17,3 +17,14 @@ export const searchForShows = query => apiGet(`/search/shows?q=${query}`); //<==
 export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
 export const getShowById = showId =>
   apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+export const getShowsByIds = async showIds => {
+  // get all the showIds and their info into one place
+  // which we are going to use as a promise
+  // these promises are not dependent on each other that's why we can handle 'em parrallely
+  const promises = showIds.map(showId => apiGet(`/shows/${showId}`));
+
+  const result = await Promise.all(promises); //resolve the whole array at once
+  // [reolve1,resolve2, null, undefined] order is retained
+  console.log('result: ', result);
+  return result; //we're gonna have an array of shows
+};
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,16 +1,54 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getShowById, getShowsByIds } from '../api/tvmaze';
+import ShowGrid from '../components/shows/ShowGrid';
 import { useStarredShows } from '../lib/useStarredShows';
 
 function Starred() {
-  const [starredShows] = useStarredShows(); //only need the data here. No action
+  //the arraay of ids in local storage
+  const [starredShowsIds] = useStarredShows(); //only need the data here. No action
 
-  return (
-    <div>
-      <h1>starred page</h1>
-      You have starred {starredShows.length} movies!
-    </div>
-  );
+  //querying the data for the starred shows
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds], //starredshows only contains the ids and we're passing all of 'em here!
+    // queryFn: () => {
+    //   //we need our result to contain show property
+    //   getShowById(starredShowsIds).then(result =>
+    //     result.map(showData => {
+    //       return { showData };
+    //     })
+    //   );
+    // },
+    queryFn: () =>
+      //we need our result to contain show property
+      getShowsByIds(starredShowsIds).then(
+        result =>
+          // result.map(showData => ({ showData })) <==Doesn't work?
+          result.map(show => ({ show })) //we should pass it 'show' it can pass the right data
+      ),
+    refetchOnWindowFocus: false,
+  });
+
+  console.log('starredShows: ', { starredShows });
+
+  if (starredShows?.length > 0) {
+    return (
+      <>
+        <p> You have starred {starredShowsIds.length} movies!</p>
+
+        <ShowGrid shows={starredShows} />
+      </>
+    );
+  }
+  if (starredShows?.length === 0) {
+    return <div>Empty here! Strange that you got nothing to star!</div>;
+  }
+  if (starredShowsError) {
+    return <div>Error!!! {starredShowsError.message}</div>;
+  }
+
+  return <div>Wait! It is loading!</div>;
 }
 
 export default Starred;
